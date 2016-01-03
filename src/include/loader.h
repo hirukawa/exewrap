2d4b2b9944eb722e0af35b500fed815ace888e91
hirukawa_ryo <hirukawa_ryo@d83a06de-854e-4732-85b7-fdf7162022b6>
2016-01-03 01:31:45 +0000

    1.1.2 *プロセスの環境変数を設定するユーティリティークラス exewrap.util.Environment を追加しました。

--- src/include/loader.h
+++ src/include/loader.h
@@ -27,16 +27,17 @@ typedef struct _LOAD_RESULT {
 extern "C" {
 #endif
 
-extern BOOL         LoadMainClass(int argc, char* argv[], char* utilities, LOAD_RESULT* result);
-extern BOOL         SetSplashScreenResource(char* splash_screen_name, BYTE* splash_screen_image_buf, DWORD splash_screen_image_len);
-extern DWORD WINAPI RemoteCallMainMethod(void* _shared_memory_handle);
-extern char*        ToString(JNIEnv* env, jobject object, char* buf);
-extern char*        GetModuleObjectName(const char* prefix);
-extern BYTE*        GetResource(LPCTSTR name, RESOURCE* resource);
-extern char*        GetWinErrorMessage(DWORD err, int* exit_code, char* buf);
-extern char*        GetJniErrorMessage(int err, int* exit_code, char* buf);
-extern void JNICALL JNI_WriteEventLog(JNIEnv *env, jobject clazz, jint logType, jstring message);
-extern void JNICALL JNI_UncaughtException(JNIEnv *env, jobject clazz, jstring thread, jstring message, jstring trace);
+extern BOOL            LoadMainClass(int argc, char* argv[], char* utilities, LOAD_RESULT* result);
+extern BOOL            SetSplashScreenResource(char* splash_screen_name, BYTE* splash_screen_image_buf, DWORD splash_screen_image_len);
+extern DWORD   WINAPI  RemoteCallMainMethod(void* _shared_memory_handle);
+extern char*           ToString(JNIEnv* env, jobject object, char* buf);
+extern char*           GetModuleObjectName(const char* prefix);
+extern BYTE*           GetResource(LPCTSTR name, RESOURCE* resource);
+extern char*           GetWinErrorMessage(DWORD err, int* exit_code, char* buf);
+extern char*           GetJniErrorMessage(int err, int* exit_code, char* buf);
+extern void    JNICALL JNI_WriteEventLog(JNIEnv *env, jobject clazz, jint logType, jstring message);
+extern void    JNICALL JNI_UncaughtException(JNIEnv *env, jobject clazz, jstring thread, jstring message, jstring trace);
+extern jstring JNICALL JNI_SetEnvironment(JNIEnv *env, jobject clazz, jstring key, jstring value);
 
 #ifdef __cplusplus
 }
--- src/java/exewrap/core/ExewrapClassLoader.java
+++ src/java/exewrap/core/ExewrapClassLoader.java
@@ -189,4 +189,5 @@ public class ExewrapClassLoader extends ClassLoader {
 	public static native void WriteConsole(byte[] b, int off, int len);
 	public static native void WriteEventLog(int type, String message);
 	public static native void UncaughtException(String thread, String message, String trace);
+	public static native String SetEnvironment(String key, String value);
 }
